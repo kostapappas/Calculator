45aba5d0d03be30856b3fae5edb27a841742e1ae
Kostapappas Lampros <>
2020-05-25 20:21:04 +0300

    init min sdk 11 Device iPhone iPad Mac

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by LAMBROS KOSTAPAPPAS on 25/5/20.
+//  Copyright © 2020 LAMBROS KOSTAPAPPAS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
